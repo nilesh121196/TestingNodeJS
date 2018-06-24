5dba06dabaac353d08759f8199c260dc3e672d43
Nilesh Mittal <nilesh121196@gmail.com>
2018-06-25 01:03:57 +0530

    1.0

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-// var http = require('http');
+var http = require('http');
 
-// // http.createServer(function (req, res) {
-// //     res.writeHead(200, {'Content-Type': 'text/plain'});
-// //     res.end('Hello World!');
-// // }).listen(8080);
-// http.createServer(function(req, res){
-// 	res.writeHead(200);
-// 	res.end("you are .....");
+// http.createServer(function (req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     res.end('Hello World!');
 // }).listen(8080);
+http.createServer(function(req, res){
+	res.writeHead(200);
+	res.end("you are .....");
+}).listen(8080);
 
-var express = require('express');
-var app = express();
+// var express = require('express');
+// var app = express();
 
-app.set('port',(process.env.PORT | 5000));
+// app.set('port',(process.env.PORT | 5000));
 
 
-app.get('/', function(req, res){
-	console.log("Hello the world of nodejs");
-	res.send("Hello");
-})
-app.listen(app.get('port'), function(){
-	console.log('App is run on port', app.get('port'));
-});
+// app.get('/', function(req, res){
+// 	console.log("Hello the world of nodejs");
+// 	res.send("Hello");
+// })
+// app.listen(app.get('port'), function(){
+// 	console.log('App is run on port', app.get('port'));
+// });
 
