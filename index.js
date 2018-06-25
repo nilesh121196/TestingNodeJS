078151eb8a37121d755567703f161514d16e604d
Nilesh Mittal <nilesh121196@gmail.com>
2018-06-25 10:31:54 +0530

    1.0

--- index.js
+++ index.js
@@ -10,15 +10,29 @@
 // }).listen(8080);
 
 var express = require('express');
-var app = express(),
-PORT = process.env.PORT || 5000
+var app = express();
+
+app.set('port',(process.env.PORT || 5000));
 
 
-app.get('/', (req, res)=>{
+app.get('/', function(req, res){
 	console.log("Hello the world of nodejs");
-	res.send("Hello");
+	res.send("Hello this is 2");
 })
-app.listen(PORT, function(){
-	console.log(`App is run on port ${PORT}`);
+app.listen(app.get('port'), function(){
+	console.log('App is run on port', app.get('port'));
 });
 
+
+// var express = require('express');
+// var app = express(),
+// PORT = process.env.PORT || 5000
+
+
+// app.get('/', (req, res)=>{
+// 	console.log("Hello the world of nodejs");
+// 	res.send("Hello");
+// })
+// app.listen(PORT, function(){
+// 	console.log(`App is run on port ${PORT}`);
+// });
